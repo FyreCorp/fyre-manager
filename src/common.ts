8cad1deb475d8a3624d589e38de3b806bcb7fe18
Liam Labell <thefallenspirit@outlook.com>
2025-08-09 15:29:47 -0700

    Add production product IDs

--- src/common.ts
+++ src/common.ts
@@ -2,8 +2,8 @@ export const defaultColor = 0x249fe6;
 const dev = process.argv.includes('--dev');
 
 export const productKeys = {
-    [dev ? "18995f5c-cf3d-4800-ba41-021e2ec4298f" : ""]: 'user-license',
-    [dev ? "4ae392e6-e3b5-4d73-ba3b-208c8aba67d2" : ""]: 'guild-license',
+    [dev ? "18995f5c-cf3d-4800-ba41-021e2ec4298f" : "d9ac98b3-b736-421a-95a7-47cc8d1dd96f"]: 'user-license',
+    [dev ? "4ae392e6-e3b5-4d73-ba3b-208c8aba67d2" : "128d9df3-bb7b-4de1-a012-3ad10aa2eb4f"]: 'guild-license',
     [dev ? "6047b376-6c56-48ab-a1c6-5da5b8d79c3f" : ""]: 'custom-branding-1',
     [dev ? "59a17c78-9622-4a84-87ef-f33aa677f4c6" : ""]: 'custom-branding-2',
     [dev ? "0c9ab719-e750-4b5d-abc8-8bd241b542be" : ""]: 'custom-branding-3',
